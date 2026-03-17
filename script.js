82b62017ee871310eb7981ba96b82b8f4b9d02e9
ChocolateScorpion <roderick.rzg@gmail.com>
2026-03-17 00:30:23 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,5 @@
 console.log("script loaded");
 
-document.addEventListener("DOMContentLoaded", () => {
-
-const button = document.getElementById("convertBtn");
-
-button.addEventListener("click", convertImage);
-
-});
-
 async function convertImage(){
 
 console.log("convertImage triggered");
@@ -15,10 +7,8 @@ console.log("convertImage triggered");
 const input = document.getElementById("heicInput");
 
 if(!input || !input.files.length){
-
 document.getElementById("status").innerText = "Please select a HEIC file.";
 return;
-
 }
 
 const file = input.files[0];
@@ -44,12 +34,10 @@ link.style.display = "block";
 
 document.getElementById("status").innerText = "Conversion complete.";
 
-}
-
-catch(error){
+}catch(error){
 
 console.error(error);
-document.getElementById("status").innerText = "Conversion failed. Make sure the file is HEIC.";
+document.getElementById("status").innerText = "Conversion failed.";
 
 }
 
